6e1d467de1a2aa6c0555f5e5664d21f3d1d9bac5
Austin0077 <austinqweyu@gmail.com>
2017-10-10 21:54:15 +0300

    Release 5

---
+++ java123.js
@@ -0,0 +1,6 @@
+<script type="text/javascript">
+		document.write("Welcome to Javascript");
+		alert("Welcome to MMU");
+		prompt("Enter your name");
+		confirm("Are sure you want to delete");
+</script>
