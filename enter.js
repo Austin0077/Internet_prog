048dbb4763f856ab2cc845c3adaa40545aab58ff
Austin0077 <austinqweyu@gmail.com>
2017-10-11 14:35:20 +0300

    Release 5.988

---
+++ enter.js
@@ -0,0 +1,14 @@
+function enternum() {
+	// body...
+	promt("Enter a Number");
+}
+function validatename(name)
+{
+	if (name=="Austin") {
+		document.write("Welcome");
+	}
+	else 
+	{
+		alert("Invalid Name!");
+	}
+}
